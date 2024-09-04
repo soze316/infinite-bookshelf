27720e99aaf0de78c99b216a8714a374ee33e35b
soze <74239824+soze316@users.noreply.github.com>
2024-09-04 22:45:47 +0000

    short story

--- infinite_bookshelf/agents/section_writer.py
+++ infinite_bookshelf/agents/section_writer.py
@@ -6,7 +6,7 @@ from ..inference import GenerationStatistics
 
 
 def generate_section(
-    prompt: str, additional_instructions: str, model: str, groq_provider, long: bool = True
+    prompt: str, additional_instructions: str, model: str, groq_provider, long: bool = False
 ):
 
     PROMPT_USER = ""
--- infinite_bookshelf/agents/structure_writer.py
+++ infinite_bookshelf/agents/structure_writer.py
@@ -10,7 +10,7 @@ def generate_book_structure(
     additional_instructions: str,
     model: str,
     groq_provider,
-    long: bool = True,
+    long: bool = False,
 ):
     """
     Returns book structure content as well as total tokens and total time for generation.
@@ -78,12 +78,9 @@ def generate_book_structure(
         #  for a long (>300 page) book. It is very important that use the following subject and additional instructions to 
         #  write the book. \n\n<subject>{prompt}</subject>\n\n<additional_instructions>{additional_instructions}</additional_instructions>"""
     else:
-        USER_PROMPT = f"""You are tasked with writing a 200-word short story. Your goal is to create an engaging and well-structured 
-        narrative that adheres to specific guidelines. Follow these instructions carefully to craft your story:
-       <subject>{prompt}</subject>
-       <aditional instructions>{additional_instructions}</additional_instructions>   
-        Write your complete short story within <story> tags.
-        Begin writing your story now. 
+        USER_PROMPT = f"""Write a comprehensive structure, omiting introduction and conclusion sections (forward, author's note, summary),
+          for a 200 word short story. It is very important that use the following subject and additional instructions to 
+          write the book. \n\n<subject>{prompt}</subject>\n\n<additional_instructions>{additional_instructions}</additional_instructions>
         """
 
     completion = groq_provider.chat.completions.create(
